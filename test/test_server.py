9347926091e2f98a2f56b4a5d4d7e3879479fe0c
Erik Reid <erik.reid@geant.org>
2017-06-30 20:37:22 +0200

    basic tests of initial server resources

---
+++ test/test_server.py
@@ -0,0 +1,66 @@
+from contextlib import contextmanager
+import filecmp
+import json
+import os
+import pkg_resources
+from StringIO import StringIO
+import shutil
+import tempfile
+import zipfile
+
+import pytest
+
+from perfsonar_data import server
+
+_HEADERS = {
+    "Content-type": "application/json",
+    "Accept": ["application/json"]
+}
+
+
+@contextmanager
+def tempdir():
+    dir_name = tempfile.mkdtemp()
+    try:
+        yield dir_name
+    finally:
+        shutil.rmtree(dir_name, ignore_errors=True)
+
+
+@pytest.fixture
+def client():
+    return server.app.test_client()
+
+
+def test_slshosts(client, db_with_test_data):
+    from perfsonar_data import proxy
+    import data
+    proxy.init_db_engine(db_with_test_data)
+
+    payload = { "url": data.SLS_BOOTSTRAP_URL }
+    rv = client.post(
+        "/slshosts",
+        data=json.dumps(payload),
+        headers=_HEADERS)
+
+    assert rv.status_code == 200
+
+    response = json.loads(rv.data)
+    assert len(response) > 0  # TODO: a better test on the response
+
+
+def test_archive_participants(client, db_with_test_data):
+    from perfsonar_data import proxy
+    import data
+    proxy.init_db_engine(db_with_test_data)
+
+    payload = { "url": data.ESMOND_BASE_URL }
+    rv = client.post(
+        "/esmond/participants",
+        data=json.dumps(payload),
+        headers=_HEADERS)
+
+    assert rv.status_code == 200
+
+    response = json.loads(rv.data)
+    assert len(response) > 0  # TODO: a better test on the response
