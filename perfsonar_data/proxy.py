01435af694bd881847fcd5324c79af77dab001d0
Erik Reid <erik.reid@geant.org>
2017-07-05 21:15:40 +0200

    added handling for docs.expires to proxy

--- perfsonar_data/proxy.py
+++ perfsonar_data/proxy.py
@@ -1,17 +1,23 @@
 import json
 import logging
+import time
 
 import requests
 
 from perfsonar_data import model
 
 
-def load_url_json(url, session):
+def load_url_json(url, session, expires=int(time.time())):
+
+    session.query(model.Doc) \
+        .filter(model.Doc.expires < int(time.time())) \
+        .delete()
+    session.commit()
 
     row = session.query(model.Doc).filter(model.Doc.url == url).first()
     if row is not None:
-        logging.debug("proxy hit for '%s'" % url)
-        return json.loads(row.doc)
+            logging.debug("proxy hit for '%s'" % url)
+            return json.loads(row.doc)
 
     logging.debug("downloading from '%s'" % url)
 
@@ -20,7 +26,7 @@ def load_url_json(url, session):
     assert "application/json" in r.headers["content-type"]
     data = r.json()
 
-    session.add(model.Doc(url=url, doc=json.dumps(data)))
+    session.add(model.Doc(url=url, expires=expires, doc=json.dumps(data)))
     session.commit()
 
     logging.debug("saved new document for '%s'" % url)
