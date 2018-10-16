11612b6b3888a795f2555ebefe7cf8ce06c68dec
Erik Reid <erik.reid@geant.org>
2018-10-16 21:50:08 +0200

    include package data in installation

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.3",
     description="esmond helper/proxy",
     packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
     install_requires=[
         "requests",
         "alembic",
