43065d1a1c23bcb11d3353b98caa7a52a336902a
Erik Reid <erik.reid@geant.org>
2018-10-16 10:18:00 +0200

    moved app_factory to __init__.py

--- perfsonar_data/app.py
+++ perfsonar_data/app.py
@@ -1,17 +1,20 @@
 """
-entry point that can be invoked as:
+testing entry point that can be invoked as:
 
     FLASK_APP=perfsonar_data.app flask run
 
 or just by running the module directly
 """
-from perfsonar_data import app_factory
-
-# TODO: take dsn & port from somewhere ...?
-
-app = app_factory.create_app()
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=8234)
+    import logging
+    import sys
+    import perfsonar_data
+
+    logging.basicConfig(
+        stream=sys.stderr,
+        level=logging.DEBUG)
+
+    app = perfsonar_data.create_app()
+    app.run(host="0.0.0.0", port="8234")
+
