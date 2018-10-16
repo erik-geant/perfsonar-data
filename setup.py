7f475f8abd42e6b7804049bfc09362a7b7512513
Erik Reid <erik.reid@geant.org>
2018-10-16 14:55:57 +0200

    don't install tests/

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="esmond_helper",
     version="0.1",
     description="esmond helper/proxy",
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
     install_requires=[
         "requests",
         "alembic",
