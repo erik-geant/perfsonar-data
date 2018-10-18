2c3875865d5a3260a06efb4ec6b8078abb704bcd
Erik Reid <erik.reid@geant.org>
2018-10-18 11:09:12 +0200

    bumped next release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esmond_helper",
-    version="0.6",
+    version="0.7",
     description="esmond helper/proxy",
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
